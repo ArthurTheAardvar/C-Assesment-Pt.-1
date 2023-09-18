84d749af9c9fac300d80e554fc22febec8731668
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2023-09-18 13:33:05 -0600

    Add project files.

---
+++ C++ Assesment Pt. 1/C++ Assesment Pt. 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	for (int i = 5; i <= 50; i += 3)
+		cout << i << " ";
+
+	cout << endl;
+
+	for (int j = 80; j >= 10; j -= 2)
+		cout << j << " ";
+
+	cout << endl;
+
+	for (int m = 2; m <= 200; m *= 5)
+		cout << m << " ";
+}
